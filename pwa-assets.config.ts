580239f1be6f7cebdc8562272f583ee4ce0dc57b
Jacob Odden <jacob.l.odden@gmail.com>
2026-07-10 20:32:05 -0400

    Add app icons

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -2,6 +2,14 @@ import { defineConfig, minimal2023Preset } from '@vite-pwa/assets-generator/conf
 
 export default defineConfig({
   headLinkOptions: { preset: '2023' },
-  preset: minimal2023Preset,
+  preset: { ...minimal2023Preset, ...{
+   transparent: {
+     sizes: [64, 192, 512 ],
+     favicons: [
+       [48, 'favicon-48x48.ico'],
+       [64, 'favicon.ico'],
+     ],
+   }
+  }},
   images: ['public/pwa-icon.png'],
 })
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'icons.svg'],
+      includeAssets: ['favicon.png'],
       manifest: {
         name: 'Cardz',
         short_name: 'Cardz',
